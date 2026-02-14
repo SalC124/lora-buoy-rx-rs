2cff69aed45b80cf18cfe36375279f17daf74471
SalC124 <Saltcal124@gmail.com>
2026-02-14 16:09:38 -0500

    man, i love examples

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,142 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise, some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use core::convert::TryInto;
+use embedded_svc::{
+    http::{Headers, Method},
+    io::{Read, Write},
+    wifi::{AuthMethod, ClientConfiguration, Configuration},
+};
+use esp_idf_svc::hal::{peripherals::Peripherals};
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    http::server::EspHttpServer,
+    nvs::EspDefaultNvsPartition,
+    wifi::{BlockingWifi, EspWifi},
+};
+use log::*;
+use serde::Deserialize;
 
-    // Bind the log crate to the ESP Logging facilities
+const SSID: &'static str = env!("SSID"); // of the wifi to connect to
+const PASSWORD: &'static str = env!("PASS");
+
+static INDEX_HTML: &str = include_str!("http_server_page.html");
+
+// Max payload length
+const MAX_LEN: usize = 128;
+
+// Need lots of stack to parse JSON
+const STACK_SIZE: usize = 10240;
+
+#[derive(Deserialize)]
+struct FormData<'a> {
+    first_name: &'a str,
+    age: u32,
+    birthplace: &'a str,
+}
+
+fn main() -> anyhow::Result<()> {
+    esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    // Setup Wifi
+    let peripherals = Peripherals::take()?;
+    let sys_loop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+
+    let mut wifi = BlockingWifi::wrap(
+        EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?,
+        sys_loop,
+    )?;
+
+    connect_wifi(&mut wifi)?;
+
+    let mut server = create_server()?;
+
+    server.fn_handler("/", Method::Get, |req| {
+        req.into_ok_response()?
+            .write_all(INDEX_HTML.as_bytes())
+            .map(|_| ())
+    })?;
+
+    server.fn_handler::<anyhow::Error, _>("/post", Method::Post, |mut req| {
+        let len = req.content_len().unwrap_or(0) as usize;
+
+        if len > MAX_LEN {
+            req.into_status_response(413)?
+                .write_all("Request too big".as_bytes())?;
+            return Ok(());
+        }
+
+        let mut buf = vec![0; len];
+        req.read_exact(&mut buf)?;
+        let mut resp = req.into_ok_response()?;
+
+        if let Ok(form) = serde_json::from_slice::<FormData>(&buf) {
+            write!(
+                resp,
+                "Hello, {}-year-old {} from {}!",
+                form.age, form.first_name, form.birthplace
+            )?;
+        } else {
+            resp.write_all("JSON error".as_bytes())?;
+        }
+
+        Ok(())
+    })?;
+
+    // Keep wifi and the server running beyond when main() returns (forever)
+    // Do not call this if you ever want to stop or access them later.
+    // Otherwise you can either add an infinite loop so the main task
+    // never returns, or you can move them to another thread.
+    // https://doc.rust-lang.org/stable/core/mem/fn.forget.html
+    core::mem::forget(wifi);
+    core::mem::forget(server);
+
+    // Main task no longer needed, free up some memory
+    Ok(())
+}
+
+fn connect_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> anyhow::Result<()> {
+    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
+        ssid: SSID.try_into().unwrap(),
+        bssid: None,
+        auth_method: AuthMethod::WPA2Personal,
+        password: PASSWORD.try_into().unwrap(),
+        channel: None,
+        ..Default::default()
+    });
+
+    wifi.set_configuration(&wifi_configuration)?;
+    wifi.start()?;
+    info!("Wifi started");
+
+    // Scan for networks
+    info!("Scanning for WiFi networks...");
+    let scan_result = wifi.scan()?;
+    for ap in scan_result {
+        info!(
+            "Found AP: SSID='{}', Signal: {}, Auth: {:?}",
+            ap.ssid, ap.signal_strength, ap.auth_method
+        );
+    }
+
+    info!("Attempting to connect...");
+    wifi.connect()?;
+    info!("Wifi connected");
+
+    wifi.wait_netif_up()?;
+    info!("Wifi netif up");
+
+    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+    info!("Wifi DHCP info: {:?}", ip_info);
+    info!("Visit http://{}", ip_info.ip);
+
+    Ok(())
+}
+
+fn create_server() -> anyhow::Result<EspHttpServer<'static>> {
+    let server_configuration = esp_idf_svc::http::server::Configuration {
+        stack_size: STACK_SIZE,
+        ..Default::default()
+    };
+
+    Ok(EspHttpServer::new(&server_configuration)?)
 }
